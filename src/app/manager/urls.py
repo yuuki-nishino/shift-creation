86494b40219d5c0ccda3ed408546e9c839e3e6a5
金子　雄真 <kanekoyuma@kanekoyuushinnoMacBook-Air.local>
2022-04-22 15:19:20 +0900

    APIを送信

--- src/app/manager/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'manager'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
--- src/app/shirotengu/admin.py
+++ src/app/shirotengu/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+# Register your models here.
 from .models import Store,User,Shift
 
 admin.site.register(Store)
